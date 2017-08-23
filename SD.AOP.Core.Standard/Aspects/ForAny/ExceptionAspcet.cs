2f972110a7795d589505f5e11739248bb38d9409
Lee <lishilei0523@163.com>
2017-08-23 20:28:20 +0800

    添加.NET Standard 2.0版本

---
+++ SD.AOP.Core.Standard/Aspects/ForAny/ExceptionAspcet.cs
@@ -0,0 +1,71 @@
+﻿using PostSharp.Aspects;
+using SD.AOP.Core.Attributes;
+using SD.AOP.Core.Interfaces;
+using SD.AOP.Core.Models.Entities;
+using SD.AOP.Core.Models.ValueObjects;
+using SD.AOP.Core.Toolkits;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace SD.AOP.Core.Aspects.ForAny
+{
+    /// <summary>
+    /// 异常AOP特性基类
+    /// </summary>
+    [Serializable]
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public abstract class ExceptionAspect : OnExceptionAspect
+    {
+        /// <summary>
+        /// 异常日志字段
+        /// </summary>
+        protected readonly ExceptionLog _exceptionLog;
+
+        /// <summary>
+        /// 异常消息
+        /// </summary>
+        protected ExceptionMessage _exceptionMessage;
+
+        /// <summary>
+        /// 构造器
+        /// </summary>
+        protected ExceptionAspect()
+        {
+            this._exceptionLog = new ExceptionLog();
+        }
+
+        /// <summary>
+        /// 异常过滤器
+        /// </summary>
+        /// <param name="eventArgs">方法元数据</param>
+        public override void OnException(MethodExecutionArgs eventArgs)
+        {
+            if (!eventArgs.Method.IsDefined(typeof(SkipExceptionAttribute)))
+            {
+                //初始化异常日志
+                this._exceptionLog.BuildBasicInfo(eventArgs);
+                this._exceptionLog.BuildMethodArgsInfo(eventArgs);
+                this._exceptionLog.BuildExceptionInfo(eventArgs);
+
+                //读取配置文件实例化日志记录提供者
+                Assembly impAssembly = Assembly.Load(LoggerProviderConfiguration.Setting.Assembly);
+                Type implType = impAssembly.GetType(LoggerProviderConfiguration.Setting.Type);
+                ILoggger loggger = (ILoggger)Activator.CreateInstance(implType);
+
+                //无需事务
+                using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Suppress))
+                {
+                    //插入数据库
+                    Guid newId = Task.Run(() => loggger.Write(this._exceptionLog)).Result;
+
+                    scope.Complete();
+
+                    //初始化异常消息
+                    this._exceptionMessage = new ExceptionMessage(eventArgs.Exception.Message, newId);
+                }
+            }
+        }
+    }
+}
---
+++ SD.AOP.Core.Standard/Aspects/ForMethod/RunningLogAspect.cs
@@ -0,0 +1,66 @@
+﻿using PostSharp.Aspects;
+using SD.AOP.Core.Interfaces;
+using SD.AOP.Core.Models.Entities;
+using SD.AOP.Core.Toolkits;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace SD.AOP.Core.Aspects.ForMethod
+{
+    /// <summary>
+    /// 程序日志AOP特性类
+    /// </summary>
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method, AllowMultiple = true)]
+    public class RunningLogAspect : OnMethodBoundaryAspect
+    {
+        /// <summary>
+        /// 程序日志字段
+        /// </summary>
+        protected readonly RunningLog _runningLog;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public RunningLogAspect()
+        {
+            this._runningLog = new RunningLog();
+        }
+
+        /// <summary>
+        /// 执行方法开始事件
+        /// </summary>
+        /// <param name="eventArgs">方法元数据</param>
+        public override void OnEntry(MethodExecutionArgs eventArgs)
+        {
+            this._runningLog.BuildRuningInfo(eventArgs);
+            this._runningLog.BuildBasicInfo(eventArgs);
+            this._runningLog.BuildMethodArgsInfo(eventArgs);
+        }
+
+        /// <summary>
+        /// 执行方法结束事件
+        /// </summary>
+        /// <param name="eventArgs">方法元数据</param>
+        public override void OnExit(MethodExecutionArgs eventArgs)
+        {
+            this._runningLog.BuildReturnValueInfo(eventArgs);
+
+            //读取配置文件实例化日志记录提供者
+            Assembly impAssembly = Assembly.Load(LoggerProviderConfiguration.Setting.Assembly);
+            Type implType = impAssembly.GetType(LoggerProviderConfiguration.Setting.Type);
+            ILoggger loggger = (ILoggger)Activator.CreateInstance(implType);
+
+            //无需事务
+            using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Suppress))
+            {
+                //持久化
+                Task.Run(() => loggger.Write(this._runningLog));
+
+                scope.Complete();
+            }
+        }
+    }
+}
